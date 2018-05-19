a99de433f1597265394d3e257d6ec2f04abd091e
hamoney9409 <hamoney9409@gmail.com>
2018-05-19 10:00:03 +0900

    글로벌 오브젝트 이식 작성자 : 하상범 커밋 내용 :  - 글로벌 오브젝트 이식  - 모든 씬에서 공통적으로 사용하고, 최초로 생성된 후 제거되지 않는 오브젝트가 필요하여 이를 구현.  - 사용법 : Prefabs/System/Preload.prefab을 씬 오브젝트에 추가하면 됨.

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -10,30 +10,8 @@ using UnityEngine.Events;
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject foePrefeb;
-    public GameObject bulletPrefeb;
-    public GameObject killEffectPrefeb;
     public static GameManager instance;
-
-    double mGameTime;
-    float timeScalebeforePause = 1.0f;
-    bool mPressCancelInput = true;
-    int mCurScreenHeight;
-    int mCurScreenWidth;
-
-    public string stageFileName;
     
-    UnityEvent<bool> m_pauseEvent;
-    UnityEvent<int, int> m_updateResolutionEvent;
-    
-    public class NewUnityEvent<T> : UnityEvent<T>
-    {
-    }
-
-    public class NewUnityEvent<T0, T1> : UnityEvent<T0, T1>
-    {
-    }
-
     void Awake()
     {
         if (GameManager.instance != null)
@@ -43,12 +21,6 @@ public class GameManager : MonoBehaviour
 
         GameManager.instance = this;
 
-        if (m_pauseEvent == null)
-            m_pauseEvent = new NewUnityEvent<bool>();
-
-        if (m_updateResolutionEvent == null)
-            m_updateResolutionEvent = new NewUnityEvent<int, int>();
-
         DatatableManager.InitDatatableManager();
         SoundManager.InitSoundManager(GetComponent<AudioSource>());
     }
@@ -56,116 +28,11 @@ public class GameManager : MonoBehaviour
     // Use this for initialization
     void Start ()
     {
-        mCurScreenHeight = Screen.height;
-        mCurScreenWidth = Screen.width;
-        // 이건 플레이씬에서 플레이매니저가 따로 해주도록 할거임;
-        // StartCoroutine(InputCoroutine());
-        
-        // Spawn Foe
-        //var position = (Vector3)UnityEngine.Random.insideUnitCircle * 6.0f;
-        //var clone = LeanPool.Spawn(foePrefeb, position, Quaternion.identity, null);
-
+        Debug.Log("GameManager 실행 완료!");
     }
 
     // Update is called once per frame
-    void Update ()
-    {
-        mGameTime += Time.deltaTime;
-        // width와 height이 동시에 바뀔 경우, height 쪽이 우선도를 갖게 하였다.
-        // height가 더 큰 게임이라 편해보이기 위함
-        if (mCurScreenHeight != Screen.height) 
-        {
-            // 8에 나눠떨어지는 값으로 수정
-            int height = Screen.height - (Screen.height % 8);
-            int width = height / 8 * 5;
-
-            ChangeResolution(width, height);
-
-        }
-        else if (mCurScreenWidth != Screen.width)
-        {
-
-            // 5에 나눠떨어지는 값으로 수정
-            int width = Screen.width - (Screen.width % 5);
-            int height = width / 5 * 8;
-
-
-            
-            ChangeResolution(width, height);
-        }
-    }
-
-    void ChangeResolution(int width, int height)
-    {
-        int maxSize = Math.Min(Screen.currentResolution.width / 5, Screen.currentResolution.height / 8);
-        width = Math.Min(width, maxSize * 5);
-        height = Math.Min(height, maxSize * 8);
-
-        Screen.SetResolution(width, height, false);
-
-        mCurScreenWidth = width;
-        mCurScreenHeight = height;
-
-        m_updateResolutionEvent.Invoke(width, height);
-    }
-
-    IEnumerator InputCoroutine()
-    {
-        while (true)
-        {
-            if (0.1 < Input.GetAxisRaw("Cancel"))
-            {
-                if (!mPressCancelInput)
-                {
-                    mPressCancelInput = true;
-                    Pause(!BasicProperties.IsPaused);
-                }
-            }
-            else
-            {
-                if (mPressCancelInput)
-                    mPressCancelInput = false;
-            }
-
-            yield return null;
-        }
-    }
-    public void AddUpdatePauseListener(UnityAction<bool> func)
-    {
-        m_pauseEvent.AddListener(func);
-    }
-
-    public void AddUpdateResolutionListener(UnityAction<int, int> func)
-    {
-        m_updateResolutionEvent.AddListener(func);
-    }
-
-    public void Pause(bool pause)
-    {
-        if (pause)
-        {
-            if (!BasicProperties.IsPaused)
-            {
-                timeScalebeforePause = Time.timeScale;
-                m_pauseEvent.Invoke(true);
-                Time.timeScale = 0;
-            }
-        }
-        else
-        {
-            if (BasicProperties.IsPaused)
-            {
-                Time.timeScale = timeScalebeforePause;
-                m_pauseEvent.Invoke(false);
-            }
-        }
-    }
-
-    public double gameTime
-    {
-        get
-        {
-            return mGameTime;
-        }
-    }
+    //void Update ()
+    //{
+    //}
 }
