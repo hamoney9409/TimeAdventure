be8f92724e2c1d91169f9ee58497db104fc62b5a
ShinSooHwan <tlstnghks11@gmail.com>
2018-05-30 21:35:32 +0900

    0523 기본적인 게임 구조 메인, 설정, 스테이지 (스테이지는 1-1만 구현 , 추후 하나씩 추가) PreFabs, Scripts 에 스테이지 구현에 필요한 요소
    작성자 : 신수환

---
+++ Assets/Scripts/QuitGame.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Application.Quit();
+	}
+
+    private void OnApplicationQuit(){
+        
+    }
+}
