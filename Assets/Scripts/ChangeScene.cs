8f0c7bd036663f01529c73e21b69e86ee959208a
ShinSooHwan <tlstnghks11@gmail.com>
2018-06-09 16:52:42 +0900

    * 0609 기본적인 게임 구조 메인, 설정, 스테이지 ( 총 6개의 월드 각 4개의 스테이지 씬 작성)
    작성자 : 신수환

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -5,13 +5,143 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour {
 
-    public void SceneChange1() {
+    public void SceneChange_main() {
         SceneManager.LoadScene("MainScene");
     }
 
-    public void SceneChange2()
-    {
-        SceneManager.LoadScene("Scene1");
+    public void SceneChange_11() {
+        SceneManager.LoadScene("Scene_11");
+    }
+    public void SceneChange_12() {
+        SceneManager.LoadScene("Scene_12");
+    }
+    public void SceneChange_13() {
+        SceneManager.LoadScene("Scene_13");
+    }
+    public void SceneChange_14() {
+        SceneManager.LoadScene("Scene_14");
+    }
+    public void SceneChange_15() {
+        SceneManager.LoadScene("Scene_15");
+    }
+    public void SceneChange_16() {
+        SceneManager.LoadScene("Scene_16");
     }
 
+
+    public void SceneChange_111()
+    {
+        SceneManager.LoadScene("Scene_111");
+    }
+    public void SceneChange_112()
+    {
+        SceneManager.LoadScene("Scene_112");
+    }
+    public void SceneChange_113()
+    {
+        SceneManager.LoadScene("Scene_113");
+    }
+    public void SceneChange_114()
+    {
+        SceneManager.LoadScene("Scene_114");
+    }
+
+
+    public void SceneChange_121()
+    {
+        SceneManager.LoadScene("Scene_121");
+    }
+    public void SceneChange_122()
+    {
+        SceneManager.LoadScene("Scene_122");
+    }
+    public void SceneChange_123()
+    {
+        SceneManager.LoadScene("Scene_123");
+    }
+    public void SceneChange_124()
+    {
+        SceneManager.LoadScene("Scene_124");
+    }
+
+
+    public void SceneChange_131()
+    {
+        SceneManager.LoadScene("Scene_131");
+    }
+    public void SceneChange_132()
+    {
+        SceneManager.LoadScene("Scene_132");
+    }
+    public void SceneChange_133()
+    {
+        SceneManager.LoadScene("Scene_133");
+    }
+    public void SceneChange_134()
+    {
+        SceneManager.LoadScene("Scene_134");
+    }
+
+
+    public void SceneChange_141()
+    {
+        SceneManager.LoadScene("Scene_141");
+    }
+    public void SceneChange_142()
+    {
+        SceneManager.LoadScene("Scene_142");
+    }
+    public void SceneChange_143()
+    {
+        SceneManager.LoadScene("Scene_143");
+    }
+    public void SceneChange_144()
+    {
+        SceneManager.LoadScene("Scene_144");
+    }
+
+
+    public void SceneChange_151()
+    {
+        SceneManager.LoadScene("Scene_151");
+    }
+    public void SceneChange_152()
+    {
+        SceneManager.LoadScene("Scene_152");
+    }
+    public void SceneChange_153()
+    {
+        SceneManager.LoadScene("Scene_153");
+    }
+    public void SceneChange_154()
+    {
+        SceneManager.LoadScene("Scene_154");
+    }
+
+  
+    public void SceneChange_161() {
+        SceneManager.LoadScene("Scene_161");
+    }
+    public void SceneChange_162() {
+        SceneManager.LoadScene("Scene_162");
+    }
+    public void SceneChange_163() {
+        SceneManager.LoadScene("Scene_163");
+    }
+    public void SceneChange_164() {
+        SceneManager.LoadScene("Scene_164");
+    }
+
+
+    public void SceneChange_21(){
+        SceneManager.LoadScene("Scene_21");
+    }
+    public void SceneChange_22(){
+        SceneManager.LoadScene("Scene_22");
+    }
+
+
+    public void SceneChange_3(){
+        SceneManager.LoadScene("Scene_3");
+    }
 }
