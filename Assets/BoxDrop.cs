3c5d266dee188630d5a4c95d3fe54d830285aec8
ShinSooHwan <tlstnghks11@gmail.com>
2018-06-11 01:37:29 +0900

    0611

---
+++ Assets/BoxDrop.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxDrop : MonoBehaviour {
+
+    public GameObject Box;
+    
+    bool isDrop = false;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && !isDrop) Drop();
+    }
+    void Drop()
+    {
+        isDrop = true;
+        Destroy(Box);
+
+}
+}
