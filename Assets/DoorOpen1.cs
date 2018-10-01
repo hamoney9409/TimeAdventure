19c3133699aba7803e9e0a4a036ed33385a13905
fastmankr <fastmankr@gmail.com>
2018-10-01 16:47:00 +0900

    순간이동, 오브젝트 파괴, 스위치

--- Assets/DoorOpen1.cs
+++ Assets/DoorOpen1.cs
@@ -4,21 +4,19 @@ using UnityEngine;
 
 public class DoorOpen1 : MonoBehaviour
 {
-
+    public int swi = 0;
     int cnt = 0;
     bool isOpen = false; 
-    private void OnTriggerEnter(Collider other) 
-    {
-        if (other.gameObject.tag == "Player" && !isOpen) Open(); 
-    }
-    void Open()
+   
+    
+    public void Open()
     {
         isOpen = true; 
         cnt++; 
         Vector3 rot = transform.eulerAngles; 
         rot.y -= 1f; 
         transform.eulerAngles = rot;
-        if (cnt < 90) Invoke("Open", Time.deltaTime); 
+        if (cnt < 45) Invoke("Open", Time.deltaTime); 
         else 
             cnt = 0; 
     }
