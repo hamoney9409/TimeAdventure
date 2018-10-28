5413c96e18fb465d9fc5500171feb914ad04d0e7
hamoney9409 <hamoney9409@gmail.com>
2018-10-28 18:55:37 +0900

    지형의 그리드 규격을 자동으로 맞추는 도구 개발
    작성자 : 하상범
    
    작업 내용 :
      - Ground오브젝트에 안에 지형지물 자식으로 다 넣고 Ground 스크립트를 매달면됨

---
+++ Assets/Scripts/DevelopTool/Ground.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground : MonoBehaviour {
+       
+    public void SetChildrenGrid()
+    {
+        Transform[] allChildren = GetComponentsInChildren<Transform>();
+        foreach (Transform child in allChildren)
+        {
+            child.position = new Vector3((int)child.position.x, (int)child.position.y, (int)child.position.z);
+        }
+    }
+       
+	// Use this for initialization
+//	void Start ()
+ //   {
+//	    
+//	}
+	
+	// Update is called once per frame
+//	void Update () {
+//		
+//	}
+}
