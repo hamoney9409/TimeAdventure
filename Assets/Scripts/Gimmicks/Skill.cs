f1829e658b6eeff1a615df5ce63d1cbfbd870842
Shinsoohwan2 <alfeelyoupda@naver.com>
2018-10-28 14:03:41 +0900

    2018.10.28
    신수환
    
    스킬 스크립트, 버튼 추가 (신 162)

---
+++ Assets/Scripts/Gimmicks/Skill.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skill : MonoBehaviour {
+
+	public GameObject tileSet_A;
+	public GameObject tileSet_B;
+
+
+
+	void Start(){
+
+		tileSet_A.SetActive (true);
+		tileSet_B.SetActive (false);
+	}
+		
+	public void TimeSkill(){
+
+		if (tileSet_A.activeSelf == true) {	// A -> B
+
+			tileSet_B.SetActive (true);
+			tileSet_A.SetActive (false);
+
+		
+		}
+
+		else if (tileSet_B.activeSelf == true) {	// B -> A
+
+			tileSet_A.SetActive (true);
+			tileSet_B.SetActive (false);
+
+		
+		}
+	}
+
+}
