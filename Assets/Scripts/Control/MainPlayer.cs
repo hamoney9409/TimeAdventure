752e299a8048fecaae8b61286b1b3bbf2837be4c
Shinsoohwan2 <alfeelyoupda@naver.com>
2018-11-13 23:14:56 +0900

    메인씬, 게임 스테이지 선택 씬 만들기
    각 스테이지의 컨셉에 맞는 필드를 지나가게 함
    그에 필요한 스크립트 2개 추가
    
    작성자 : 신수환

---
+++ Assets/Scripts/Control/MainPlayer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainPlayer : MonoBehaviour {
+
+    public int Speed;
+   
+    private Vector3 vec;
+    private int count = 0;
+
+   
+
+	// Use this for initialization
+	void Start () {
+
+        Animation ani = gameObject.GetComponent<Animation>();
+        ani.Play("walk");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.forward * Speed * Time.deltaTime);
+
+    }
+
+    private void OnTriggerEnter(Collider other){
+        if (other.tag == "trigger") {
+            count++;
+            vec = transform.position;
+            vec.x = vec.x + 60;
+
+            if (count == 6){
+                vec.x = 0;
+                count = 0;
+            }
+
+            transform.position = vec;
+  
+        }
+    }
+
+}
